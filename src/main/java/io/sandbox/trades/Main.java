ffd4a4dff3e05727bb26c93301daf5b8c85ee197
Tyler Crocker <crocker.tylerj@gmail.com>
2022-09-28 22:34:35 -0700

    updating to new logger

--- src/main/java/io/sandbox/trades/Main.java
+++ src/main/java/io/sandbox/trades/Main.java
@@ -1,12 +1,15 @@
 package io.sandbox.trades;
 
+import io.sandbox.lib.SandboxLogger;
 import io.sandbox.trades.items.ItemLoader;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
+	private static final SandboxLogger LOGGER = new SandboxLogger("SandboxTrades");
+
 	@Override
 	public void onInitialize() {
 		ItemLoader.init();
-		System.out.println("Initializing Sandbox Biome Trades");
+		LOGGER.info("Initialized");
 	}
 }
